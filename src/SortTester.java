d73872e7bf569c3afb137ef883d94677abfc8168
Nandhini Namasivayam <nnamasivayam@menloschool.org>
2025-02-27 21:12:04 -0800

    Updated names and test results

--- src/SortTester.java
+++ src/SortTester.java
@@ -1,6 +1,16 @@
+/**
+ * A file to test the sort functions using the same
+ * provided data. The size of the data is stored in
+ * SortingAlgorithms.java so that students need only
+ * modify a single file.
+ *
+ * @author: Nandhini Namasivayam
+ * @version: February 2024
+ */
+
 import java.util.Random;
 
-public class Main {
+public class SortTester {
     /**
      * Tests bubble sort without modifying data
      * @param data
@@ -12,12 +22,17 @@ public class Main {
         // Check if it's sorted
         System.out.print("Bubble Sort: ");
         if(!SortTester.isSorted(data)) {
-            System.out.println("FAILED");
+            System.out.println("FAILED - output:");
+            printArray(data);
         } else {
             System.out.println("PASSED");
         }
     }
 
+    /**
+     * Tests selection sort without modifying data
+     * @param data
+     */
     public static void testSelectionSort(int[] data) {
         data = data.clone();
         SortingAlgorithms.selectionSort(data);
@@ -25,12 +40,17 @@ public class Main {
         // Check if it's sorted
         System.out.print("Selection Sort: ");
         if(!SortTester.isSorted(data)) {
-            System.out.println("FAILED");
+            System.out.println("FAILED - output:");
+            printArray(data);
         } else {
             System.out.println("PASSED");
         }
     }
 
+    /**
+     * Tests merge sort without modifying data
+     * @param data
+     */
     public static void testMergeSort(int[] data) {
         data = data.clone();
         SortingAlgorithms.mergeSort(data);
@@ -38,10 +58,44 @@ public class Main {
         // Check if it's sorted
         System.out.print("Merge Sort: ");
         if(!SortTester.isSorted(data)) {
-            System.out.println("FAILED");
+            System.out.println("FAILED - output:");
+            printArray(data);
         } else {
             System.out.println("PASSED");
         }
+    }
+
+    // Generate a random array of a given size
+    private static int[] generateRandomArray(int size) {
+        Random rand = new Random();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = rand.nextInt(SortingAlgorithms.MAX_NUMBER_SIZE); // Random numbers between 0 and 9999
+        }
+        return arr;
+    }
+
+    /**
+     * Determines if the array is sorted
+     * in ascending order
+     * @param arr
+     * @return True if sorted and False other
+     */
+    public static boolean isSorted(int[] arr) {
+        for(int i=0; i<arr.length-1; i++) {
+            if(arr[i] > arr[i+1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Utility method to print an array
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
@@ -55,16 +109,7 @@ public class Main {
         testSelectionSort(data);
 
         // Test Merge Sort
-        SortingAlgorithms.mergeSort(data);
-    }
-
-    // Generate a random array of a given size
-    private static int[] generateRandomArray(int size) {
-        Random rand = new Random();
-        int[] arr = new int[size];
-        for (int i = 0; i < size; i++) {
-            arr[i] = rand.nextInt(10000); // Random numbers between 0 and 9999
-        }
-        return arr;
+        // TODO: Uncomment to run this test
+//        testMergeSort(data);
     }
 }
--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -1,2 +1,51 @@
-public class Sorts {
+/**
+ * A class to implement all the sorting algorithms
+ * so that we can test timing.
+ *
+ * @author: Nandhini Namasivayam
+ * @version February 2025
+ */
+public class SortingAlgorithms {
+    // Change this value to change testing size
+    public static final int TEST_SIZE = 10;
+
+    // Change this value to change the range of numbers generated
+    public static final int MAX_NUMBER_SIZE = 100;
+
+    // Bubble Sort
+    public static void bubbleSort(int[] arr) {
+        // TODO: Implement bubble sort
+    }
+
+    // Selection Sort
+    public static void selectionSort(int[] arr) {
+        // TODO: Implement selection sort
+    }
+
+    // Merge Sort
+    public static void mergeSort(int[] arr) {
+        // TODO: Implement merge sort
+    }
+
+    private static void mergeSortHelper(int[] arr, int left, int right) {
+        // TODO: Implement the recursive helper for merge sort
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        // TODO: Implement the merge step for merge sort
+    }
+
+    // Quick Sort
+    public static void quickSort(int[] arr) {
+        // TODO: Implement quick sort
+    }
+
+    private static void quickSortHelper(int[] arr, int low, int high) {
+        // TODO: Implement the recursive helper for quick sort
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        // TODO: Implement the partition step for quick sort
+        return 0; // Placeholder
+    }
 }
